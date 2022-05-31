3115cb8314999b4b8e016c00308ace2bfd147523
olakrhoff <ola.kr@hotmail.no>
2022-05-31 19:13:10 +0200

    kake

--- day_19/main.cpp
+++ day_19/main.cpp
@@ -2,161 +2,285 @@
 #include <fstream>
 #include <utility>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-#define DEBUG 0
+#define DEBUG 1
 
-struct snail_number_t
+struct coord_t
 {
-    string number {};
+    int16_t x {}, y {}, z {};
     
-    explicit snail_number_t(string number) : number(std::move(number))
+    coord_t()
     {}
     
-    void make_number_valid()
-    {
-        while (true)
-        {
-            if (explode())
-                continue;
-            if (split())
-                continue;
-            break;
-        }
-    }
+    coord_t(int16_t x, int16_t y, int16_t z) : x(x), y(y), z(z)
+    {}
     
-    bool explode()
+    explicit coord_t(const string &values)
     {
-        uint8_t depth {};
-        for (uint16_t i = 0; i < number.size(); ++i)
-        {
-            if (number[i] == '[')
-                ++depth;
-            else if (number[i] == ']')
-                --depth;
-            
-            if (depth > 4)
-            {
-                uint16_t end_index = i + 1;
-                while (number[end_index] != ']')
-                    ++end_index;
-                
-                string exploding_number = number.substr(i,  end_index - i + 1);
-                string left_number = exploding_number.substr(1, exploding_number.find(',') - 1);
-                string right_number = exploding_number.substr(exploding_number.find(',') + 1,  exploding_number.size() - exploding_number.find(',') - 2);
-                
-                uint8_t left {(uint8_t)stoi(left_number)}, right {(uint8_t)stoi(right_number)};
-                
-                number.replace(i, end_index - i + 1, "0");
-                
-                string left_part_of_number = number.substr(0, i), right_part_of_number = number.substr(i);
-                for (int32_t j = left_part_of_number.size() - 1; j >= 0; --j) //add the left value to the next number to the left
-                {
-                    if (left_part_of_number.at(j) != '[' && left_part_of_number.at(j) != ']' && left_part_of_number.at(j) != ',')
-                    {
-                        uint8_t length_of_digit {1};
-                        uint8_t old_left {(uint8_t)(left_part_of_number.at(j) - '0')};
-                        if (j > 1 && left_part_of_number.at(j - 1) != '[' && left_part_of_number.at(j - 1) != ']' && left_part_of_number.at(j - 1) != ',')
-                        {
-                            uint8_t temp {(uint8_t)(left_part_of_number.at(j - 1) - '0')};
-                            old_left = temp * 10 + old_left;
-                            ++length_of_digit;
-                            --j;
-                        }
-                        
-                        left_part_of_number.replace(j, length_of_digit, to_string(old_left + left));
-                        break;
-                    }
-                }
-                
-                for (int32_t j = 1; j < right_part_of_number.size(); ++j) //add the right value to the next number to the right
-                {
-                    if (right_part_of_number.at(j) != '[' && right_part_of_number.at(j) != ']' && right_part_of_number.at(j) != ',')
-                    {
-                        uint8_t length_of_digit {1};
-                        uint8_t old_right {(uint8_t)(right_part_of_number.at(j) - '0')};
-                        if (j < right_part_of_number.size() - 1 && right_part_of_number.at(j + 1) != '[' && right_part_of_number.at(j + 1) != ']' && right_part_of_number.at(j + 1) != ',')
-                        {
-                            uint8_t temp {(uint8_t)(right_part_of_number.at(j + 1) - '0')};
-                            old_right = old_right * 10 + temp;
-                            ++length_of_digit;
-                        }
-                        
-                        right_part_of_number.replace(j, length_of_digit, to_string(old_right + right));
-                        break;
-                    }
-                }
-                number = left_part_of_number + right_part_of_number;
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    bool split()
-    {
-        for (uint32_t i = 0; i < number.size(); ++i)
-        {
-            if (number.at(i) != '[' && number.at(i) != ']' && number.at(i) != ',')
-            {
-                uint8_t value {(uint8_t)(number.at(i) - '0')};
-                if (i < number.size() - 1 && number.at(i + 1) != '[' && number.at(i + 1) != ']' && number.at(i + 1) != ',')
-                    value = value * 10 + (uint8_t)(number.at(i + 1) - '0');
-                else
-                    continue;
-                
-                uint8_t left = value / 2, right = value - left;
-                number.replace(i, 2, string("[" + to_string(left) + "," + to_string(right) + "]"));
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    uint64_t get_magnitude() const
-    {
-        string num = number;
+        string x_string = values.substr(0, values.find_first_of(','));
+        string y_string = values.substr(values.find_first_of(',') + 1,
+                                        values.find_last_of(',') - x_string.length() - 1);
+        string z_string = values.substr(values.find_last_of(',') + 1);
         
-        for (uint16_t index = num.find_first_of(']'); index != USHRT_MAX; index = num.find_first_of(']'))
-        {
-            uint16_t start_bracket = index;
-            while (num.at(start_bracket) != '[')
-                --start_bracket;
-            
-            string exploding_number = num.substr(start_bracket,  index - start_bracket + 1);
-            string left_number = exploding_number.substr(1, exploding_number.find(',') - 1);
-            string right_number = exploding_number.substr(exploding_number.find(',') + 1,  exploding_number.size() - exploding_number.find(',') - 2);
-            
-            uint64_t left {(uint64_t)stoi(left_number)}, right {(uint64_t)stoi(right_number)};
-            uint64_t magnitude = 3 * left + 2 * right;
-            
-            num.replace(start_bracket, index - start_bracket + 1, to_string(magnitude));
-        }
-        
-        return stoi(num);
+        x = (int16_t) stoi(x_string);
+        y = (int16_t) stoi(y_string);
+        z = (int16_t) stoi(z_string);
     }
     
-    snail_number_t &operator+=(const snail_number_t &other)
+    bool operator<(const coord_t &rhs) const
     {
-        number = "[" + number + "," + other.number + "]";
-        make_number_valid();
+        if (x < rhs.x)
+            return true;
+        if (rhs.x < x)
+            return false;
+        if (y < rhs.y)
+            return true;
+        if (rhs.y < y)
+            return false;
+        return z < rhs.z;
+    }
+    
+    bool operator>(const coord_t &rhs) const
+    {
+        return rhs < *this;
+    }
+    
+    bool operator<=(const coord_t &rhs) const
+    {
+        return !(rhs < *this);
+    }
+    
+    bool operator>=(const coord_t &rhs) const
+    {
+        return !(*this < rhs);
+    }
+    
+    bool operator==(const coord_t &rhs) const
+    {
+        return x == rhs.x &&
+               y == rhs.y &&
+               z == rhs.z;
+    }
+    
+    bool operator!=(const coord_t &rhs) const
+    {
+        return !(rhs == *this);
+    }
+    
+    coord_t &operator+=(const coord_t &other)
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        
         return *this;
+    };
+    
+    coord_t &operator-=(const coord_t &other)
+    {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        
+        return *this;
+    };
+    
+    coord_t operator-(const coord_t &other) const
+    {
+        coord_t res {*this};
+        
+        res.x -= other.x;
+        res.y -= other.y;
+        res.z -= other.z;
+        
+        return res;
     }
     
-    snail_number_t operator+(const snail_number_t &other)
+    friend ostream &operator<<(ostream &os, const coord_t &coord)
     {
-        snail_number_t new_number("[" + number + "," + other.number + "]");
-        new_number.make_number_valid();
-     
-        return new_number;
-    }
-    
-    friend ostream &operator<<(ostream &os, const snail_number_t &snail_number)
-    {
-        return os << snail_number.number;
+        return os << coord.x << "," << coord.y << "," << coord.z;
     }
 };
+
+struct cube_t
+{
+    private:
+        
+        //      | /
+        //      |/
+        // -----o----- X
+        //     /|
+        //    / |
+        //   Z  Y
+        //
+        //X: width, Y: height, Z: depth
+        enum ROTATE
+        {
+            X, Y, Z
+        };
+        
+        uint8_t number_of_rotates {};
+        enum POSITIONS
+        {
+            F, R, U, L, D, B
+        } pos;
+    public:
+        coord_t scanner {};
+        set<coord_t> sensors {};
+    
+    public:
+        void next_orientation()
+        {
+            rotate(Z);
+            if (++number_of_rotates == 4)
+            {
+                number_of_rotates = 0;
+                if (pos == F || pos == L)
+                {
+                    rotate(Y);
+                    if (pos == F)
+                        pos = R;
+                    else
+                        pos = B;
+                }
+                else if (pos == R || pos == D || pos == B || pos == U)
+                {
+                    rotate(X);
+                    switch (pos)
+                    {
+                        case R:
+                            pos = D;
+                            break;
+                        case D:
+                            pos = L;
+                            break;
+                        case B:
+                            pos = U;
+                            break;
+                        case U:
+                            pos = F;
+                            break;
+                    }
+                }
+                else
+                {
+                    cout << "Bad switch" << endl;
+                    exit(1);
+                }
+            }
+        }
+        
+        pair<coord_t, int8_t> is_overlapping(const cube_t &other) const
+        {
+            const uint8_t MINIMUM_OVERLAPPING_SENSORS = 12;
+            cube_t temp = other;
+            for (int i = 0; i < 24; ++i) //Loop through all the possible orientations
+            {
+                for (coord_t sensor : temp.sensors)
+                {
+                    for (coord_t fixed_sensor : sensors)
+                    {
+                        cube_t cube_orientation = temp;
+                        //fixed_sensor + diff = sensor
+                        //diff = sensor - fixed_sensor
+                        coord_t diff = sensor - fixed_sensor;
+                        cube_orientation.translate(diff, (int8_t)i);
+                        
+                        uint8_t overlapping_sensors {};
+                        auto itr = cube_orientation.sensors.begin();
+                        for (int j = 0; j < cube_orientation.sensors.size(); ++j)
+                        {
+                            if (sensors.contains(*itr))
+                            {
+                                if (++overlapping_sensors >= MINIMUM_OVERLAPPING_SENSORS)
+                                    break;
+                            }
+                            else if (cube_orientation.sensors.size() - j - 1 < MINIMUM_OVERLAPPING_SENSORS - overlapping_sensors)
+                                break;
+                            itr++;
+                        }
+                        if (overlapping_sensors >= MINIMUM_OVERLAPPING_SENSORS)
+                            return make_pair(diff, i);
+                    }
+                }
+                temp.next_orientation();
+            }
+            return make_pair(coord_t(), -1);
+        }
+        
+        void combine(const cube_t &other, pair<coord_t, int8_t> overlap)
+        {
+            cube_t temp = other;
+            temp.translate(overlap.first, overlap.second);
+            
+            for (coord_t coord : temp.sensors)
+                sensors.insert(coord);
+        }
+    
+    private:
+        void translate(const coord_t &difference, int8_t orientation)
+        {
+            for (int8_t i = 0; i < orientation; ++i)
+                next_orientation();
+    
+            vector<coord_t> sensors_temp {sensors.begin(), sensors.end()};
+            
+            for (coord_t &coord : sensors_temp)
+                coord -= difference;
+            
+            sensors = {sensors_temp.begin(), sensors_temp.end()};
+            
+            scanner -= difference;
+        }
+        
+        //Rotates the cube clockwise with the axis chosen.
+        void rotate(ROTATE dir)
+        {
+            vector<coord_t> sensors_temp {sensors.begin(), sensors.end()};
+            switch (dir)
+            {
+                case X:
+                    scanner = coord_t(scanner.x, -scanner.z, scanner.y);
+                    for (coord_t &coord : sensors_temp)
+                        coord = coord_t(coord.x, -coord.z, coord.y);
+                    break;
+                case Y:
+                    scanner = coord_t(scanner.z, scanner.y, -scanner.x);
+                    for (coord_t &coord : sensors_temp)
+                        coord = coord_t(coord.z, coord.y, -coord.x);
+                    break;
+                case Z:
+                    scanner = coord_t(-scanner.y, scanner.x, scanner.z);
+                    for (coord_t &coord : sensors_temp)
+                        coord = coord_t(-coord.y, coord.x, coord.z);
+                    break;
+                default:
+                    cout << "Bad switch dir" << endl;
+                    exit(1);
+            }
+            sensors = {sensors_temp.begin(), sensors_temp.end()};
+        }
+    
+    public:
+        friend ostream &operator<<(ostream &os, const cube_t &cube)
+        {
+            for (coord_t coord : cube.sensors)
+                os << coord << "\n";
+            return os;
+        }
+};
+
+template<typename T>
+T pop_front(vector<T> &vec)
+{
+    T front = vec.at(0);
+    vec = {vec.begin() + 1, vec.end()};
+    
+    return front;
+}
 
 int main()
 {
@@ -170,36 +294,47 @@ int main()
     if (!file.is_open())
         return EXIT_FAILURE;
     
-    vector<snail_number_t> numbers {};
+    
+    vector<cube_t> cubes {};
+    
     string temp {};
     while (!file.eof())
     {
         file >> temp;
-        numbers.emplace_back(temp);
+        if (temp.find(',') == -1)
+        {
+            if (temp[0] >= '0' && temp[0] <= '9')
+                cubes.emplace_back(cube_t());
+            continue;
+        }
+        cubes.at(cubes.size() - 1).sensors.insert(coord_t(temp));
     }
     file.close();
     
-    snail_number_t sum {*numbers.begin()};
-    
-    for (int i = 1; i < numbers.size(); ++i)
-        sum += numbers.at(i);
-    
-    cout << sum.get_magnitude() << endl;
-    
-    uint64_t max_magnitude {0};
-    for (int i = 0; i < numbers.size(); ++i)
+    /*
+    for (int i = 0; i < 24; ++i)
     {
-        for (int j = i + 1; j < numbers.size(); ++j)
+        cout << cubes.at(0) << "\n\n";
+        cubes.at(0).next_orientation();
+    }
+    */
+    
+    cube_t complete_map = pop_front(cubes);
+    
+    
+    while (!cubes.empty())
+    {
+        cube_t next = pop_front(cubes);
+        auto overlap = complete_map.is_overlapping(next);
+        if (overlap.second == -1)
         {
-            uint64_t val = (numbers.at(i) + numbers.at(j)).get_magnitude();
-            max_magnitude = max_magnitude < val ? val : max_magnitude;
-            
-            val = (numbers.at(j) + numbers.at(i)).get_magnitude();
-            max_magnitude = max_magnitude < val ? val : max_magnitude;
+            cubes.emplace_back(next);
+            continue;
         }
+        complete_map.combine(next, overlap);
     }
     
-    cout << max_magnitude << endl;
+    cout << complete_map.sensors.size() << endl;
     
     return EXIT_SUCCESS;
 }
